c2bc4b0537068a3a56091a6bf776ee6933f59b16
KaveenW <117672888+KaveenW@users.noreply.github.com>
2026-01-15 22:21:30 +0530

    Initial commit with the Editor

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TextEditor from "./TextEditor.jsx";
+const App = () => {
+  return (
+    <div>
+      <TextEditor />
+    </div>
+  );
+};
+
+export default App;
